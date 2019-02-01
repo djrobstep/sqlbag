a63380751fa187f3c273b01bb3aa17f58ae67777
Robert Lechte <robertlechte@gmail.com>
2019-02-01 15:17:44 +1100

    vbump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open("README.rst") as f:
 
 setup(
     name="sqlbag",
-    version="0.1.1541129705",
+    version="0.1.1548994599",
     url="https://github.com/djrobstep/sqlbag",
     description="various snippets of SQL-related boilerplate",
     long_description=readme,
